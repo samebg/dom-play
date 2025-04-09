a704933b5eac78feb108b40dd40471b3a4ac72bd
samebg <samebengho@gmail.com>
2025-04-09 11:45:45 -0700

    DOne

---
+++ js/dom-play.js
@@ -0,0 +1,57 @@
+//alert("hi");
+
+/*
+
+With a single click, highlight (bold or otherwise) all lines for a specific actor in a play
+
+Un-highlight (normal or otherwise) all lines from all other actors in the play
+
+The function(s) created to do the work must accommodate any number of actors, and any number of additional lines added to the page
+
+The page must do the work without page re-loads (i.e. it must be done via JavaScript and the DOM)
+
+The page may not use a third party API (jQuery, for example)
+
+HTML "hooks" may be added to the page, meaning you can edit the HTML to enable your scripts to perform properly
+
+
+*/
+
+
+//document.querySelector(".hamlet").style.backgroundColor = "yellow";
+
+
+const spans = document.querySelectorAll("div#play span");
+
+console.log(spans);
+
+for (const mySpan of spans){
+    mySpan.addEventListener("click", function(ev){
+        let myActor = mySpan.dataset.actor;
+        highlightActor(myActor);
+    });
+}
+
+function highlightActor(myActor){
+    for(const mySpan of spans){
+        if(myActor == mySpan.dataset.actor){
+            mySpan.style.backgroundColor = "orange";
+    
+        }else{
+        mySpan.style.backgroundColor = "white";
+    
+        }
+    }
+}
+
+function highlight(el){
+
+    if(el.style.backgroundColor == "orange"){
+        el.style.backgroundColor = "white";
+
+    }else{
+        el.style.backgroundColor = "orange";
+
+    }
+
+}
